1860fcc389945a272d110a9356b93bbd1ffde594
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-12-04 01:07:09 -0200

    Add main menu

--- app/src/main/java/org/burguer/poli/poliburguer/activity/Login.java
+++ app/src/main/java/org/burguer/poli/poliburguer/activity/Login.java
@@ -91,7 +91,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login);
 
         Button login = (Button)findViewById(R.id.loginButton);
         login.setOnClickListener(mLoginClickListener);
