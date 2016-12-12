eca1920ac7c2d66898858f43e54e4c0fb7602f57
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-12-11 23:44:52 -0200

    Fix notification POST and display

--- app/src/main/java/org/burguer/poli/poliburguer/firebase/NetworkOperation.java
+++ app/src/main/java/org/burguer/poli/poliburguer/firebase/NetworkOperation.java
@@ -1,27 +1,28 @@
 package org.burguer.poli.poliburguer.firebase;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 
 
 public class NetworkOperation extends AsyncTask<String, String, String> {
 
     @Override
-    protected String doInBackground(String ...destinationApp){
+    protected String doInBackground(String... destinationApp){
         HttpURLConnection connection = null;
 
         try {
-            //Create connection
             URL url = new URL("https://fcm.googleapis.com/fcm/send");
-            connection = (HttpURLConnection) url.openConnection();
+            connection = (HttpURLConnection)url.openConnection();
             connection.setRequestMethod("POST");
-            connection.setRequestProperty("Authorization: key=AAAA7Ejacp8:APA91bFqWFLb8cTkhcFwyl7jMUhemI-UZzkNCL3W9Tqb0sHd0D7dDcBE4urHrIL_31Zzrqa7IhM434JTUEzxWnEJz_z-y9kDxDZUHCD9qZ_cXHUI34oPxvXZ2HUzm6cTldSlv3CCLJ-VMnS22KdWzfsU1HWiHdD1CA", "");
+            connection.setRequestProperty("Authorization", "key=AAAA7Ejacp8:APA91bFqWFLb8cTkhcFwyl7jMUhemI-UZzkNCL3W9Tqb0sHd0D7dDcBE4urHrIL_31Zzrqa7IhM434JTUEzxWnEJz_z-y9kDxDZUHCD9qZ_cXHUI34oPxvXZ2HUzm6cTldSlv3CCLJ-VMnS22KdWzfsU1HWiHdD1CA");
             connection.setRequestProperty("Content-Type", "application/json");
             connection.setUseCaches(false);
             connection.setDoOutput(true);
@@ -32,20 +33,27 @@ public class NetworkOperation extends AsyncTask<String, String, String> {
             notification.put("title", "Poli Burguer");
             notification.put("body", "Seu Pedido está pronto!");
             parent.put("notification", notification);
-            parent.put("to", destinationApp);
-
+            parent.put("to", destinationApp[0]);
 
             OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream());
-            os.write(URLEncoder.encode(parent.toString(), "UTF-8"));
+            os.write(parent.toString());
             os.close();
-            System.out.print("Teste");
 
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null)
+                response.append(inputLine);
+            in.close();
+
+            Log.i("PoliBurguer", "FCM response: " + response.toString());
+            Log.i("PoliBurguer", "FCM Status: " + connection.getResponseCode());
+            assert(connection.getResponseCode() == 200);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (connection != null) {
+            if (connection != null)
                 connection.disconnect();
-            }
         }
         return null;
     }
--- app/src/main/java/org/burguer/poli/poliburguer/firebase/NotificationsService.java
+++ app/src/main/java/org/burguer/poli/poliburguer/firebase/NotificationsService.java
@@ -14,17 +14,6 @@ import com.google.firebase.messaging.RemoteMessage;
 
 import org.burguer.poli.poliburguer.R;
 import org.burguer.poli.poliburguer.activity.Login;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 
 public class NotificationsService extends FirebaseMessagingService {
 
@@ -38,18 +27,6 @@ public class NotificationsService extends FirebaseMessagingService {
     // [START receive_message]
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // [START_EXCLUDE]
-        // There are two types of messages data messages and notification messages. Data messages are handled
-        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
-        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
-        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
-        // When the user taps on the notification they are returned to the app. Messages containing both notification
-        // and data payloads are treated as notification messages. The Firebase console always sends notification
-        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
         // Check if message contains a data payload.
@@ -60,76 +37,29 @@ public class NotificationsService extends FirebaseMessagingService {
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getNotification().getBody());
         }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
     // [END receive_message]
 
-//    /**
-//     * Send a FCM message.
-//     *
-//     * @param destinationApp FCM destination.
-//     */
-//    public static void sendNotification(String destinationApp) {
-//        HttpURLConnection connection = null;
-//
-//        try {
-//            //Create connection
-//            URL url = new URL("https://fcm.googleapis.com/fcm/send");
-//            connection = (HttpURLConnection) url.openConnection();
-//            connection.setRequestMethod("POST");
-//            connection.setRequestProperty("Authorization: key=AAAA7Ejacp8:APA91bFqWFLb8cTkhcFwyl7jMUhemI-UZzkNCL3W9Tqb0sHd0D7dDcBE4urHrIL_31Zzrqa7IhM434JTUEzxWnEJz_z-y9kDxDZUHCD9qZ_cXHUI34oPxvXZ2HUzm6cTldSlv3CCLJ-VMnS22KdWzfsU1HWiHdD1CA", "");
-//            connection.setRequestProperty("Content-Type", "application/json");
-//            connection.setUseCaches(false);
-//            connection.setDoOutput(true);
-//
-//            JSONObject notification =new JSONObject();
-//            JSONObject parent =new JSONObject();
-//
-//            notification.put("title", "Poli Burguer");
-//            notification.put("body", "Seu Pedido está pronto!");
-//            parent.put("notification", notification);
-//            parent.put("to", destinationApp);
-//
-//
-//            OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream());
-//            os.write(URLEncoder.encode(parent.toString(),"UTF-8"));
-//            os.close();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return ;
-//        } finally {
-//            if (connection != null) {
-//                connection.disconnect();
-//            }
-//        }
-//    }
-//    /**
-//     * Create and show a simple notification containing the received FCM message.
-//     *
-//     * @param messageBody FCM message body received.
-//     */
-//    private void sendNotification(String messageBody) {
-//        Intent intent = new Intent(this, Login.class);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-//                PendingIntent.FLAG_ONE_SHOT);
-//
-//        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-//                .setSmallIcon(R.drawable.ic_stat_ic_notification)
-//                .setContentTitle("FCM Message")
-//                .setContentText(messageBody)
-//                .setAutoCancel(true)
-//                .setSound(defaultSoundUri)
-//                .setContentIntent(pendingIntent);
-//
-//        NotificationManager notificationManager =
-//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-//
-//        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
-//    }
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, Login.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                .setContentTitle("FCM Message")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
 }
